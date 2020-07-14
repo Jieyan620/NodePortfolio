ab3dc8e817e42d3a31141366fa7b4be003c63b7b
Jie Yan <jieyan620@gmail.com>
2020-07-13 21:51:04 -0700

    add hbs css

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+app.set('view engine', 'hbs')
+
 app.get('/', function (req, res) {
-  res.send('Hello World 11')
+  res.render('index', {
+    title: "home",
+    name: "Jie"
+  })
+})
+
+app.get('/about', function (req, res) {
+  res.render('about', {
+    title: "home",
+    name: "Jie"
+  })
 })
 
 app.listen(3000)
