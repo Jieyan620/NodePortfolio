627df7f2b75f1c1d368059d5b3e80b3765465005
Jie Yan <jieyan620@gmail.com>
2020-07-15 21:35:42 -0700

    port

--- src/app.js
+++ src/app.js
@@ -18,6 +18,4 @@ app.get('/about', function (req, res) {
   })
 })
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("up and running on port " + process.env.PORT);
-});
+app.listen(process.env.PORT || 3000);
